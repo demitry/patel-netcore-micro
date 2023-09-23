22aa4e046f8c5fd1b7e6cdfc01734123405a794d
demitry <dpoluektov@gmail.com>
2023-09-23 13:54:37 +0300

    Consuming Product API  [72]  [73]

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -10,14 +10,17 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<IProductService, ProductService>();
 builder.Services.AddHttpClient<ICouponService, CouponService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 
+ServiceUrls.ProductAPIBase= builder.Configuration["ServiceUrls:ProductAPI"];
 ServiceUrls.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 ServiceUrls.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 
 builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
