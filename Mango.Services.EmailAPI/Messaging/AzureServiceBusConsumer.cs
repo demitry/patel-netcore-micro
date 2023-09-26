ee2b14da89cc01c3705da3a9e14ec9da6ff6588d
demitry <dpoluektov@gmail.com>
2023-09-26 14:46:02 +0300

    Register Methods to Processor [110]

--- Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,8 +1,12 @@
-﻿using Azure.Messaging.ServiceBus;
+﻿using System.Text;
+using System.Text.Json.Serialization;
+using Azure.Messaging.ServiceBus;
+using Mango.Services.EmailAPI.Models.Dto;
+using Newtonsoft.Json;
 
 namespace Mango.Services.EmailAPI.Messaging;
 
-public class AzureServiceBusConsumer
+public class AzureServiceBusConsumer : IAzureServiceBusConsumer
 {
     private readonly string serviceBusConnectionString;
     private readonly string emailCartQueue;
@@ -23,5 +27,44 @@ public class AzureServiceBusConsumer
 
         var client = new ServiceBusClient(serviceBusConnectionString);
         _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+    }
+
+    public async Task Start()
+    {
+        _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+        _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+    }
+
+    public async Task Stop()
+    {
+        _emailCartProcessor.StopProcessingAsync();
+        _emailCartProcessor.DisposeAsync();
+    }
+    
+    private Task ErrorHandler(ProcessErrorEventArgs arg)
+    {
+        Console.WriteLine(arg.Exception.ToString());
+        return Task.CompletedTask;
+    }
+
+    private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs arg)
+    {
+        // this is where we receive the message
+        var message = arg.Message;
+        var body = Encoding.UTF8.GetString(message.Body);
+        CartDto objMessage = JsonConvert.DeserializeObject<CartDto>(body);
+
+        try
+        {
+            //TODO: try to log the message
+            
+            await arg.CompleteMessageAsync(arg.Message);
+            // Hey, this message is processed and you can remove it from your queue
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
     }
 }
